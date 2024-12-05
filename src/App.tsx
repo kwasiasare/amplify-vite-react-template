f67f2928489ab3390aa600d566c7c144a6e4905a
kwasiasare <kwasi.spreadcom@gmail.com>
2024-12-05 07:11:50 -0500

    added user authentication isolation

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,12 @@ function App() {
     client.models.Todo.delete({ id })
   }
 
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   return (
     <main>
       <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
